46a012e548b0c6c6d3756c7c146ab59c0aeb2785
modernmaster <me@jamesmcguigan.co.uk>
2026-02-18 20:08:06 +0000

    Updates

--- src/Battle.ts
+++
@@ -1,7 +0,0 @@
-import App from "./App";
-
-function Battle(agressor:string, defender: string) {
-    return false;
-}
-
-export default Battle;
--- src/Countries.ts
+++
@@ -1,6 +0,0 @@
-export const Countries = [
-        { Key: 1, Value: 'USA' },
-        { Key: 2, Value: 'Russia' },
-        { Key: 3, Value: 'UK' }
-]
-export default Countries;
